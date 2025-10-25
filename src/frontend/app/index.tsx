d9e6a6ca5db495c5f1337b292fd5148ce0794480
tidoakatytek <krzysiokozak09@gmail.com>
2025-10-25 16:25:36 +0200

    Initial map, colorscheme change functionality

--- src/frontend/app/index.tsx
+++ src/frontend/app/index.tsx
@@ -1,6 +1,8 @@
 import { Button } from '@/components/ui/button';
 import { Icon } from '@/components/ui/icon';
 import { Text } from '@/components/ui/text';
+import { Map } from '@/components/map-components/map';
+import MapView, { Region } from 'react-native-maps';
 import { Link, Stack } from 'expo-router';
 import { MoonStarIcon, StarIcon, SunIcon } from 'lucide-react-native';
 import { useColorScheme } from 'nativewind';
@@ -25,32 +27,30 @@ const IMAGE_STYLE: ImageStyle = {
 
 export default function Screen() {
   const { colorScheme } = useColorScheme();
+  const mapRef = React.useRef<MapView>(null);
+
+  const [region, setRegion] = React.useState<Region>({
+    latitude: 50.054343,
+    longitude: 19.936744,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
 
   return (
     <>
       <Stack.Screen options={SCREEN_OPTIONS} />
-      <View className="flex-1 items-center justify-center gap-8 p-4">
-        <Image source={LOGO[colorScheme ?? 'light']} style={IMAGE_STYLE} resizeMode="contain" />
-        <View className="gap-2 p-4">
-          <Text className="ios:text-foreground font-mono text-sm text-muted-foreground">
-            1. Edit <Text variant="code">app/index.tsx</Text> to get started.
-          </Text>
-          <Text className="ios:text-foreground font-mono text-sm text-muted-foreground">
-            2. Save to see your changes instantly.
-          </Text>
-        </View>
-        <View className="flex-row gap-2">
-          <Link href="https://reactnativereusables.com" asChild>
-            <Button>
-              <Text>Browse the Docs</Text>
-            </Button>
-          </Link>
-          <Link href="https://github.com/founded-labs/react-native-reusables" asChild>
-            <Button variant="ghost">
-              <Text>Star the Repo</Text>
-              <Icon as={StarIcon} />
-            </Button>
-          </Link>
+      <View className="flex-1 bg-background p-4">
+        <View className="h-full w-full">
+          <Map
+            ref={mapRef}
+            variant="outline"
+            rounded="lg"
+            className="h-full w-full"
+            region={region}
+            onRegionChangeComplete={setRegion}
+            tileServer={colorScheme === 'dark' ? 'dark-nolabels' : 'carto-light'}
+          >
+          </Map>
         </View>
       </View>
     </>
