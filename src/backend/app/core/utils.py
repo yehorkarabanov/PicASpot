704753368fa8051c363a5794a40ceb71558237b4
Denys Shevchenko <denshevchenko19@gmail.com>
2025-10-26 15:00:16 +0100

    Add utils function to create users on app startup

--- src/backend/app/core/utils.py
+++ src/backend/app/core/utils.py
@@ -1,1 +1,11 @@
-pass
+async def generate_users() -> None:
+    from app.database.manager import async_session_maker
+    from app.user.models import User
+    from app.user.repository import UserRepository
+    from app.user.service import UserService
+
+    async with async_session_maker() as session:
+        user_repo = UserRepository(session=session, model=User)
+        user_service = UserService(user_repository=user_repo)
+        await user_service.create_default_users()
+
