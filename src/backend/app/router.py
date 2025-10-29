3265b585d04f49c5d6ed74a1a64825a395e8623f
Yehor Karabanov <egor.karabanov@gmail.com>
2025-10-29 14:25:51 +0100

    Add area router and include in main router

--- src/backend/app/router.py
+++ src/backend/app/router.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
+from app.area.router import router as area_router
 from app.auth.router import router as auth_router
-from app.user.router import router as users_router
+from app.user.router import router as user_router
 
 router = APIRouter()
 router.include_router(auth_router)
-router.include_router(users_router)
+router.include_router(user_router)
+router.include_router(area_router)
